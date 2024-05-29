f7627eb347f2b9582014e09445d2c88cb240bb90
python4k <python4k@vk.com>
2024-05-30 01:41:58 +0300

    Lesson 5

--- fastapi_course/main.py
+++ fastapi_course/main.py
@@ -1,6 +1,13 @@
 from datetime import date
 from typing import Optional
 from fastapi import FastAPI, Query
+from pydantic import BaseModel
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
 
 app = FastAPI()
 
@@ -13,3 +20,8 @@ def root():
 @app.get("/hotels")
 def get_hotels(location: str, date_from: date, date_to: date, stars: Optional[int] = Query(None, ge=1, le=5), has_spa: Optional[bool] = None):
     return { "location": location, "date_from": date_from, "date_to": date_to, "stars": stars, "has_spa": has_spa }
+
+
+@app.post("/bookings")
+def add_booking(booking: SBooking):
+    pass
