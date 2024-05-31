7210d2aec5dab2f13668279fe5b666068d96ee86
python4k <python4k@vk.com>
2024-05-31 21:47:06 +0300

    Lesson 6

--- fastapi_course/main.py
+++ fastapi_course/main.py
@@ -3,6 +3,34 @@ from typing import Optional
 from fastapi import FastAPI, Query
 from pydantic import BaseModel
 
+hotels = [
+    {
+        "address": "221B Baker Street",
+        "name": "The Ritz-Carlton, London",
+        "stars": 5
+    },
+    {
+        "address": "123 London Road",
+        "name": "Grand Hyatt London",
+        "stars": 4
+    },
+    {
+        "address": "123B Baker Street",
+        "name": "Hilton London",
+        "stars": 3
+    },
+    {
+        "address": "12 London Road",
+        "name": "Holiday Inn London",
+        "stars": 4
+    }
+]
+
+class SHotel(BaseModel):
+    address: str
+    name: str
+    stars: int
+    
 
 class SBooking(BaseModel):
     room_id: int
@@ -18,8 +46,13 @@ def root():
 
 
 @app.get("/hotels")
-def get_hotels(location: str, date_from: date, date_to: date, stars: Optional[int] = Query(None, ge=1, le=5), has_spa: Optional[bool] = None):
-    return { "location": location, "date_from": date_from, "date_to": date_to, "stars": stars, "has_spa": has_spa }
+def get_hotels(location: str,
+               date_from: date,
+               date_to: date,
+               stars: Optional[int] = Query(None, ge=1, le=5),
+               has_spa: Optional[bool] = None
+) -> list[SHotel]:
+    return hotels
 
 
 @app.post("/bookings")
